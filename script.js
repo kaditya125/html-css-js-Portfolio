a6df0e6979ffb219ecb007b5eb37efb34c6e680f
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-07 04:12:06 +0530

    updated

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var typingEffect  = new Typed(".typedText",{
+    string: ["Java Developer","Frontend Developer","Backend Developer","Full Stack Developer"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:2000
+
+})
