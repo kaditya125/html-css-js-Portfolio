c5e36fe2a98773a701bddc77e3224410c019322e
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-07 14:27:13 +0530

    updated

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+document.querySelector('.contact-icon').addEventListener('click', function() {
+    window.open('https://wa.me/9102202267'); 
+  });
+// var typingEffect  = new Typed(".typedText",{
+//     string: ["Java Developer","Frontend Developer","Backend Developer","Full Stack Developer"],
+//     loop:true,
+//     typeSpeed:100,
+//     backSpeed:80,
+//     backDelay:2000
 
-var typingEffect  = new Typed(".typedText",{
-    string: ["Java Developer","Frontend Developer","Backend Developer","Full Stack Developer"],
-    loop:true,
-    typeSpeed:100,
-    backSpeed:80,
-    backDelay:2000
-
-})
+// })
