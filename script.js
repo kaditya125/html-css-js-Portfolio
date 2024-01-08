5bac7dfa5ac3d7180e0f605a22ba5db120cf2f13
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-08 13:30:13 +0530

    u

--- script.js
+++ script.js
@@ -127,19 +127,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 // Add this code to your existing JavaScript or create a new script
-document.addEventListener("DOMContentLoaded", function () {
-  const feedbackForm = document.getElementById("feedbackForm");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const feedbackForm = document.getElementById("feedbackForm");
 
-  feedbackForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+//   feedbackForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
     
-    // You can replace this alert with your desired action (e.g., sending feedback to a server)
-    alert("Feedback submitted! Thank you for sharing your thoughts.");
+//     // You can replace this alert with your desired action (e.g., sending feedback to a server)
+//     alert("Feedback submitted! Thank you for sharing your thoughts.");
     
-    // Clear form fields
-    feedbackForm.reset();
-  });
-});
+//     // Clear form fields
+//     feedbackForm.reset();
+//   });
+// });
   
   
   
